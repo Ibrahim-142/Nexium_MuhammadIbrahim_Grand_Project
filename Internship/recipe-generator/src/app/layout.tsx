4e8de347931e5dc08df9f422dcad6176a557a12a
cypheribbi <bsef22m547@pucit.edu.pk>
2025-07-20 20:51:37 +0500

    minor change

--- Internship/recipe-generator/src/app/layout.tsx
+++ Internship/recipe-generator/src/app/layout.tsx
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className="scroll-smooth" suppressHydrationWarning>
+    <html lang="en" data-scroll-behavior="smooth" className="scroll-smooth" suppressHydrationWarning>
       <body
         className={`
           ${geistSans.variable} ${geistMono.variable}
